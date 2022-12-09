835aec850d1b577483dc7da13eb4867633779e32
aktar <samimaktar0773@gmail.com>
2022-12-09 15:12:46 +0530

    4-mongodb and mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect')
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +16,7 @@ app.use("/api/products", products_routes)
 
 const start = async () => {
     try {
+        await connectDB();
         app.listen(PORT, () => {
             console.log(`${PORT} I am connected..`);
         })

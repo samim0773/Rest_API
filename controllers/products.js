8c576fe432fe812ee0801f5225d3db742e3fd01f
aktar <samimaktar0773@gmail.com>
2022-12-14 23:19:04 +0530

    10-add company filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,18 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
+    const { company } = req.query;
+    const queryObject = {};
+    if (company) {
+        queryObject.company = company;
+    }
     const myData = await Product.find(req.query);
     res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
     const myData = await Product.find(req.query);
-    res.status(200).json({myData})
+    res.status(200).json({ myData })
 };
 
 module.exports = { getAllProducts, getAllProductsTesting }

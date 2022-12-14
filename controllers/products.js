6e1519f55dbffbc6087709db5287d296c8fd658f
aktar <samimaktar0773@gmail.com>
2022-12-14 22:02:53 +0530

    9-add filtration and searching

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find({});
+    const myData = await Product.find(req.query);
     res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ msg: 'get all products testing' })
+    const myData = await Product.find(req.query);
+    res.status(200).json({myData})
 };
 
 module.exports = { getAllProducts, getAllProductsTesting }
